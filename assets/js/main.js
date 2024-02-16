6317e72579c2a3860b8d7b60e1fa6e0f0cf5a945
Mario <mfercob430@g.educaand.es>
2024-02-16 11:51:56 +0100

    cambios html css js 07

--- assets/js/main.js
+++ assets/js/main.js
@@ -212,20 +212,7 @@
   /**
    * Events slider
    */
-  new Swiper('.events-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
+ 
 
   /**
    * Testimonials slider
